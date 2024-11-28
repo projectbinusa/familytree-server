dbf8613d046e359e51bbb431238bbb6c0ab146d9
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-11-28 08:02:05 +0700

    first commit

---
+++ src/test/java/com/project/familytree/FamilytreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.familytree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilytreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
